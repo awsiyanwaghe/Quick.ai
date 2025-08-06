8a9fead9d0230fec1e13077df705185a45329182
Awsiyan waghe <awsiyanw@gmail.com>
2025-08-06 11:07:22 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ await connectCloudinary()
 const port = process.env.PORT || 3000
 
 
-app.use(cors())
+app.use(cors({
+    origin:['*','https://quick-ai-1.onrender.com/'],
+    methods:['GET','POST','PATCH',"PUT",'DELETE'],
+    credentials:true
+}))
+
 app.use(express.json())
 app.use(clerkMiddleware())
 
